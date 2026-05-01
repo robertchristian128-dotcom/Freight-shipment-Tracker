8d8ddb860c2b3cc9385a492514e05c66cbaace54
robertchristian128-dotcom <robertchristian128@gmail.com>
2026-05-01 17:52:36 +0100

    Removed config.py from tracking

--- config.py
+++
@@ -1,8 +0,0 @@
-EMAIL_ADDRESS = "robertchristian128@gmail.com"
-EMAIL_PASSWORD = "ylzxgrkvflshophl"
-MANAGER_EMAIL = "x89logistics@outlook.com"
-SPREADSHEET_ID = "1p1uLcVRrbOmU7u9O1II5XQzQhVjFL9HP_hmrsI-MO6E"
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-
-]
